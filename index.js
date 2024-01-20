7f660cd60c42fc56a2dae6e69cd9fbdb148a8435
Saud Ahmed <saudahmed96x@gmail.com>
2024-01-20 13:30:21 -0500

    Add CORS middleware

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5050;
 
+app.use(cors());
+
 app.use(express.json());
 
-const warehouseRoutes = require('./routes/warehouseRoute');
-const inventoryRoutes = require('./routes/inventoryRoute');
+const warehouseRoutes = require("./routes/warehouseRoute");
+const inventoryRoutes = require("./routes/inventoryRoute");
 
-app.get('/', (_req, res) => {
+app.get("/", (_req, res) => {
   res.send("Welcome to my API");
 });
 
-app.use('/warehouses', warehouseRoutes);
-app.use('/inventories', inventoryRoutes);
+app.use("/warehouses", warehouseRoutes);
+app.use("/inventories", inventoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
